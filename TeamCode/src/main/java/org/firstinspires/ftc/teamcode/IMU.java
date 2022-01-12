f5808ebe303b5d2259db416cca9ec5de0e8c4f7e
superhvarn <56454934+superhvarn@users.noreply.github.com>
2022-01-12 13:16:54 -0500

    Create IMU.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IMU.java
@@ -0,0 +1,52 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+public class IMU {
+
+    BNO055IMU imu;
+    Orientation angles;
+    static final double Kp = .001;
+
+    public IMU(HardwareMap hardwareMap) {
+
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(initialize());
+
+        angles = imu.getAngularOrientation();
+
+    }
+
+    public static double normalizeAngle(double angle) {
+        if (angle < -180) {
+            return normalizeAngle(angle + 360);
+        } else if (angle > 180) {
+            return normalizeAngle(angle - 360);
+        }
+        return angle;
+    }
+
+    public double getAngle() {
+        return angles.firstAngle;
+    }
+
+    public void update() {
+        angles = imu.getAngularOrientation();
+    }
+
+    BNO055IMU.Parameters initialize() {
+
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json";
+        parameters.loggingEnabled = true;
+        parameters.loggingTag = "IMU";
+
+        return parameters;
+
+    }
+
+}
